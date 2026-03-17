6daec3793ecb24234b52aeb343a146231839ba76
Alessandro Demela <alessandro.demela@gmail.com>
2026-03-17 13:04:24 +0100

    Fix TypeScript conflict: Omit 'content' from ListItemProps

--- src/components/ui/ListItem.tsx
+++ src/components/ui/ListItem.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-export interface ListItemProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface ListItemProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'content'> {
   left: React.ReactNode;
   content: React.ReactNode;
   right?: React.ReactNode;
